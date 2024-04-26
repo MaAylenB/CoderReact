90e801b30081b82722f4498426899d9972903049
Maria Aylen <maaylenbenavidez@gmail.com>
2024-04-26 07:44:02 -0300

    firebase

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,17 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDnrXcfm2DFzzbMwVd2ty4oydZ9qSnGiB8",
-  authDomain: "carpishop-eb6ea.firebaseapp.com",
-  projectId: "carpishop-eb6ea",
-  storageBucket: "carpishop-eb6ea.appspot.com",
-  messagingSenderId: "380430211990",
-  appId: "1:380430211990:web:ad8ce8cba96769b84bdbb7"
+  apiKey: "AIzaSyDh0go58lHLFh90-xHfTz4hEZe7DdJV25M",
+  authDomain: "mi-app-react-11566.firebaseapp.com",
+  projectId: "mi-app-react-11566",
+  storageBucket: "mi-app-react-11566.appspot.com",
+  messagingSenderId: "239131187229",
+  appId: "1:239131187229:web:880df0a2ec8b52e56f8ef4",
+  measurementId: "G-N9TQ0S7M06"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
